28fec476cd005a8c66cfa1ac399d9fe1e3578e85
mykhailopulov <mihail.pulov.247@gmail.com>
2026-02-04 14:33:41 +0200

    Updated Student and cleared UniversityService.java

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,18 @@
-class Student extends Person {
+public class Student extends Person {
     private int course;
     private int group;
-    private String faculty;
 
-    public Student(String name) {
+    public Student(String name, int course, int group) {
         super(name);
+        this.course = course;
+        this.group = group;
     }
 
-    public int getCourse() {return course;}
-    public int getGroup() {return group;}
-    public String getFaculty() {return faculty;}
+    public int getCourse() { return course; }
+    public int getGroup() { return group; }
+
+    @Override
+    public String toString() {
+        return getName() + " (Course: " + course + ", Group: " + group + ")";
+    }
 }
