a8c12f8e32262e609f713b44c3f838ffbfd938e0
mykhailopulov <mihail.pulov.247@gmail.com>
2026-02-04 23:11:20 +0200

    Added few stats to University.java as in description

--- src/University.java
+++ src/University.java
@@ -3,6 +3,10 @@ import java.util.List;
 
 public class University {
     private List<Faculty> faculties = new ArrayList<>();
+    private final String universityFullName = "National University Kyiv Mohyla Academy";
+    private final String universityShortName = "NaUKMA";
+    private final String universityCity = "Kyiv";
+    private final String universityAddress = "2 Hryhoriya Skovorody Str";
 
     public List<Faculty> getFaculties() {
         return faculties;
