686760c76559b7f6d1d0340bcc084bdaf9add90f
mykhailopulov <mihail.pulov.247@gmail.com>
2026-03-30 16:36:18 +0300

    refactor: Group rewritten with lombok

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -1,18 +1,14 @@
+import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
+// Group entity
+@Data
 public class Group {
     private int groupNumber;
     private List<Student> students = new ArrayList<>();
 
     public Group(int groupNumber) {
         this.groupNumber = groupNumber;
-    }
-
-    public int getGroupNumber() { return groupNumber; }
-
-
-    public List<Student> getStudents() {
-        return students;
     }
 }
