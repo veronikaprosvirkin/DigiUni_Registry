e4f87950b595718de5f2edf19ed530411da7428d
Veronika <veronika.prosvirkin@gmail.com>
2026-04-23 14:43:54 +0300

    feat: implement Serializable interface for UniversityInfo record and update compiler configuration

--- src/main/java/university/UniversityInfo.java
+++ src/main/java/university/UniversityInfo.java
@@ -2,13 +2,14 @@ package university;
 
 import utils.annotations.DetailDisplay;
 
+import java.io.Serializable;
+
 public record UniversityInfo(
-            @DetailDisplay(label = "Full Name") String fullName,
-            @DetailDisplay(label = "Short Name") String shortName,
-            @DetailDisplay(label = "City") String city,
-            @DetailDisplay(label = "Address") String address
-    )
-{
+        @DetailDisplay(label = "Full Name") String fullName,
+        @DetailDisplay(label = "Short Name") String shortName,
+        @DetailDisplay(label = "City") String city,
+        @DetailDisplay(label = "Address") String address
+) implements Serializable {
     @Override
     public String toString() {
         return fullName + " (" + shortName + ") - " + city + ", " + address;
